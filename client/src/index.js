782c95686c6bb04058db43c0827fb32a651a6a89
Emmanuel Egomson <fegomson@gmail.com>
2022-02-28 17:32:17 -0600

    Setup redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import "./index.css";
 import App from "./App";
 
+// user reducer function
+const authReducer = (state = {}, action) => {
+  switch (action.type) {
+    case "LOGGED_IN_USER":
+      return {
+        ...state,
+        ...action.payload,
+      };
+    case "LOGOUT":
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+// Combine multiple reducers
+const rootReducer = combineReducers({
+  user: authReducer,
+});
+
+// Create store
+const store = createStore(rootReducer, composeWithDevTools());
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>,
   document.getElementById("root")
 );
