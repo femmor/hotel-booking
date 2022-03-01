eca357008762fecebc40422c5e989860e5412ff8
Emmanuel Egomson <fegomson@gmail.com>
2022-03-01 16:32:23 -0600

    Setup mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,28 @@
 import express from "express";
 import { readdirSync } from "fs";
-import dotenv from "dotenv";
+require("dotenv").config({ path: "./.env" });
+import cors from "cors";
+import mongoose from "mongoose";
 
 const morgan = require("morgan");
 
 const app = express();
 
-dotenv.config();
+// Database connection
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose
+  .connect(process.env.DATABASE_URI, options)
+  .then(() => console.log("Database connection established successfully..."))
+  .catch(err => console.log(err.message));
 
 // middlewares
+app.use(cors());
 app.use(morgan("dev"));
+app.use(express.json());
 
 // route middleware
 readdirSync("./routes").map(r => app.use("/api", require(`./routes/${r}`)));
