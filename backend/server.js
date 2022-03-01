b63f149d29c21f199b12e5a56a5f139c871e2b9c
Emmanuel Egomson <fegomson@gmail.com>
2022-03-01 13:05:29 -0600

    Use morgan

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
 import express from "express";
 import { readdirSync } from "fs";
+import dotenv from "dotenv";
+
+const morgan = require("morgan");
 
 const app = express();
+
+dotenv.config();
+
+// middlewares
+app.use(morgan("dev"));
 
 // route middleware
 readdirSync("./routes").map(r => app.use("/api", require(`./routes/${r}`)));
 
-const port = 3005 || process.env.PORT;
+const port = process.env.PORT || 3005;
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
