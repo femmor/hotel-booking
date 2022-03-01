0941ec0b803b8cbac012f6a1d93630c347de75a4
Emmanuel Egomson <fegomson@gmail.com>
2022-02-28 18:29:30 -0600

    Implements express server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,13 @@
+import express from "express";
 
+const app = express();
+
+app.get("/api/:message", (req, res) => {
+  res.status(200).send(req.params.message);
+});
+
+const port = 3005 || process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
