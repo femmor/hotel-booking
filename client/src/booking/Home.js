fcc1d99fa087f0f8542cc86896f313915d1e28f1
Emmanuel Egomson <fegomson@gmail.com>
2022-02-28 18:19:26 -0600

    Server setup

--- client/src/booking/Home.js
+++ client/src/booking/Home.js
@@ -3,11 +3,7 @@ import { useSelector } from "react-redux";
 
 const Home = () => {
   const { user } = useSelector(state => ({ ...state }));
-  return (
-    <div className="container-fluid p-5 h1 text-center">
-      Homepage {JSON.stringify(user)}
-    </div>
-  );
+  return <div className="container-fluid p-5 h1 text-center">Homepage</div>;
 };
 
 export default Home;
