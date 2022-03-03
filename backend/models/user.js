a5947d5a1041d24ae554ceb0bbf2390f31d1e223
Emmanuel Egomson <fegomson@gmail.com>
2022-03-03 17:49:03 -0600

    Bcrypt hash passowrd

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
 const { Schema } = mongoose;
 
 const userSchema = new Schema(
@@ -28,6 +30,34 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+// hash password when user is created and when user is updated
+userSchema.pre("save", function (next) {
+  let user = this; // this is the user object
+  // only hash the password if it has been modified (or is new)
+  // if (!user.isModified("password")) return next(); // if password is not modified, skip this step
+  // bcrypt.genSalt(10, (err, salt) => {
+  //   if (err) return next(err);
+  //   bcrypt.hash(user.password, salt, (err, hash) => {
+  //     if (err) return next(err);
+  //     user.password = hash;
+  //     next();
+  //   });
+  // });
+
+  if (user.isModified("password")) {
+    return bcrypt.hash(user.password, 12, (err, hash) => {
+      if (err) {
+        console.log("BCRYPT HAS ERROR!", err);
+        return next(err);
+      }
+      user.password = hash;
+      return next();
+    });
+  } else {
+    return next();
+  }
+});
 
 const user = mongoose.model("User", userSchema);
 
