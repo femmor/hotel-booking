42611cf32d0c649195c5ccd41d4994804fe910af
Emmanuel Egomson <fegomson@gmail.com>
2022-03-04 13:20:11 -0600

    Save user in DB

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,7 +1,37 @@
+import User from "../models/user";
+
 export const showMessage = (req, res) => {
   res.status(200).send(req.params.message);
 };
 
-export const registerUser = (req, res) => {
-  console.log(req.body);
+export const registerUser = async (req, res) => {
+  const { name, email, password } = req.body; // destructuring req.body
+  // validation
+  if (!name) return res.status(400).send("Name is required");
+  if (!password || password.length < 6)
+    return res
+      .status(400)
+      .send("Password is required and must be at least 6 characters long");
+
+  // check if user with email already exists
+  let userExist = await User.findOne({ email }).exec();
+  if (userExist)
+    return res.status(400).send("User with the email already exists");
+
+  // if user does not exist, create new user
+  const newUser = new User({
+    // create new user object
+    name,
+    email,
+    password,
+  });
+
+  try {
+    await newUser.save(); // save user to database
+    console.log("New user created: ", newUser);
+    return res.json({ ok: true }); // send response
+  } catch (error) {
+    console.log("Create user error: ", error);
+    return res.status(400).send("Error creating user, please try again");
+  }
 };
--- backend/models/user.js
+++ backend/models/user.js
@@ -34,6 +34,7 @@ const userSchema = new Schema(
 // hash password when user is created and when user is updated
 userSchema.pre("save", function (next) {
   let user = this; // this is the user object
+
   // only hash the password if it has been modified (or is new)
   // if (!user.isModified("password")) return next(); // if password is not modified, skip this step
   // bcrypt.genSalt(10, (err, salt) => {
