152ef601b9cf7fa519305e95c6a22e3dc9a4cf99
Emmanuel Egomson <fegomson@gmail.com>
2022-03-15 18:40:01 -0500

    Conditional rendering and logout functionality

--- client/src/booking/Home.js
+++ client/src/booking/Home.js
@@ -3,7 +3,6 @@ import { useSelector } from "react-redux";
 
 const Home = () => {
   const { auth } = useSelector(state => ({ ...state }));
-  console.log(auth.user);
   return <div className="container-fluid p-5 h1 text-center">Homepage</div>;
 };
 
--- client/src/components/TopNav.js
+++ client/src/components/TopNav.js
@@ -1,18 +1,46 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
 const TopNav = () => {
+  const { auth } = useSelector(state => ({ ...state }));
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  // Logout user
+  const handleLogout = () => {
+    // empty user from redux state
+    dispatch({
+      type: "LOGOUT",
+      payload: null,
+    });
+    window.localStorage.setItem("auth", null);
+    navigate("/login");
+  };
+
   return (
     <div className="nav bg-light d-flex justify-content-between">
-      <Link className="nav-link" to="/">
-        Home
-      </Link>
-      <Link className="nav-link" to="/login">
-        Login
-      </Link>
-      <Link className="nav-link" to="/register">
-        Register
-      </Link>
+      <>
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+        {auth !== null && (
+          <a className="nav-link" onClick={handleLogout}>
+            Logout
+          </a>
+        )}
+
+        {auth === null && (
+          <>
+            <Link className="nav-link" to="/login">
+              Login
+            </Link>
+            <Link className="nav-link" to="/register">
+              Register
+            </Link>
+          </>
+        )}
+      </>
     </div>
   );
 };
