26ef439890b384473a35df0b1d6af9dbaf43ba81
Emmanuel Egomson <fegomson@gmail.com>
2022-03-15 16:24:11 -0500

    Persist redux state on page reload

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,5 +1,13 @@
+// Get user information from local storage
+let userState;
+if (localStorage.getItem("auth")) {
+  userState = JSON.parse(localStorage.getItem("auth"));
+} else {
+  userState = null;
+}
+
 // user reducer function
-export const authReducer = (state = {}, action) => {
+export const authReducer = (state = userState, action) => {
   switch (action.type) {
     case "LOGGED_IN_USER":
       return {
